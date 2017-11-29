721a31c7d6397cd783ca06f85f6d0903ce622744
Huaa <wu_zhonghua@outlook.com>
2017-11-29 14:40:59 +0800

    add second project(learning second chapter, java library)

---
+++ Second/src/com/string/ConCat.java
@@ -0,0 +1,14 @@
+package com.string;
+
+public class ConCat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String longStr = "This could have been " + "a very long line that would hava "
+				+ "wrapped around, But string concatenation " + "prevents this.";
+
+		System.out.println(longStr);
+	}
+
+}
